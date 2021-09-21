71e124d28f040293978565062c614f1b1ee3d567
Eliza Milburn <61363431+ERmilburn02@users.noreply.github.com>
2021-09-21 15:45:29 +0100

    Utils: Load Image

--- Client/src/Utils.ts
+++ Client/src/Utils.ts
@@ -4,8 +4,27 @@ import Vec2 from "./Vec2";
  * Collection of Utility functions
  */
 export default class Utils {
+    /**
+     * Get the relative mouse position from the given element
+     * @param e {MouseEvent} Mouse event to check
+     * @param element {HTMLElement} Element to check
+     * @returns {Vec2} The position of the mouse relative to the element
+     */
     static getRelativeMousePosition(e: MouseEvent, element: HTMLElement): Vec2 {
         var _rect = element.getBoundingClientRect();
         return new Vec2(Math.round(e.clientX - _rect.left), Math.round(e.clientY - _rect.top));
+    }
+
+    /**
+     * Load an image from a given url and return a promise that will be resolved with the image
+     * @param url {string} The url to load
+     * @returns {Promise<HTMLImageElement>} The loaded image
+     */
+    static loadImage(url: string): Promise<HTMLImageElement> {
+        return new Promise((resolve) => {
+            const image = new Image();
+            image.addEventListener("load", () => resolve(image));
+            image.src = url;
+        });
     }
 }
